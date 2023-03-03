709c4aa4cc3098d66b8d2d6ecc0a38361790b8e6
DESKTOP-NC63SS6\NTH <hungtienxinh1@gmail.com>
2023-03-04 01:47:19 +0700

    fix constant of LIST_POSITION_ENEMY

--- Assets/ToadJumping/Scripts/Constant.cs
+++ Assets/ToadJumping/Scripts/Constant.cs
@@ -7,9 +7,9 @@ namespace Assets.ToadJumping.Scripts
     public class Constant
     {
         public static readonly List<Vector2> LIST_POSITION_ENEMY = new() {
-            new Vector2() { x = -2, y = 8 },
-            new Vector2() { x = 0, y = 8 },
-            new Vector2() { x = 2, y = 8 }
+            new Vector2() { x = -1.87f, y = 6 },
+            new Vector2() { x = 0, y = 6 },
+            new Vector2() { x = 1.87f, y = 6 }
         };
 
         public static readonly List<Vector2> LIST_POSITION_WARNING = new() {
