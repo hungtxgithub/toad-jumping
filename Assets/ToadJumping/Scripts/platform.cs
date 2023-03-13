4ccf9888a18aeb7914d1b4b418a88d639fc78070
DESKTOP-NC63SS6\NTH <hungtienxinh1@gmail.com>
2023-03-14 02:26:56 +0700

    Update thuật toán random đệm

--- Assets/ToadJumping/Scripts/platform.cs
+++ Assets/ToadJumping/Scripts/platform.cs
@@ -37,30 +37,129 @@ public class Platform : MonoBehaviour
         Destroy(gameObject);
     }
 
-
     public void RandomStartPlatform(List<PlatformObjectVM> listPlatform)
     {
         float posY = -4;
-        var listLastPlatform = new List<PlatformOutVM>();
+        var listLastPlatform = new PlatformObjectVM[3];
+
         for (int i = 0; i < Constant.RowOfPlatform; i++)
         {
-            int numberItemInRow = Random.Range(1, 4);
-            List<GameObject> gameObjects =
-                listPlatform.Where(x => x.IsNormal == true).Select(x => x.GameObject).ToList();
+            var listPlatformInRow = new PlatformObjectVM[3];
+            int numberPlatformInRow = Random.Range(1, 4);
 
-            for (int j = 0; j < numberItemInRow - 1; j++)
-                gameObjects.Add(listPlatform[Random.Range(0, listPlatform.Count)].GameObject);
+            if (i == 0)
+            {
+                listPlatformInRow[0] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+                for (int k = 1; k < numberPlatformInRow; k++)
+                    listPlatformInRow[k] = listPlatform[Random.Range(0, listPlatform.Count)];
+            }
+            else
+            {
+                if (listLastPlatform[0] != null && listLastPlatform[0].IsNormal == true)
+                    listPlatformInRow = RandomPlatformPos0(listPlatform, numberPlatformInRow);
+                else if (listLastPlatform[1] != null && listLastPlatform[1].IsNormal == true)
+                    listPlatformInRow = RandomPlatformPos1(listPlatform, numberPlatformInRow);
+                else if (listLastPlatform[2] != null && listLastPlatform[2].IsNormal == true)
+                    listPlatformInRow = RandomPlatformPos2(listPlatform, numberPlatformInRow);
+            }
 
-            gameObjects = gameObjects.OrderBy(x => System.Guid.NewGuid()).ToList();
+            listLastPlatform = listPlatformInRow;
 
             float posX = -2;
-            for (int j = 0; j < numberItemInRow; j++)
+            for (int j = 0; j < 3; j++)
             {
-                Common.Instance.SpawnObject(gameObjects[j], new Vector2(posX, posY));
+                if (listPlatformInRow[j] != null)
+                {
+                    Common.Instance.SpawnObject(listPlatformInRow[j].GameObject, new Vector2(posX, posY));
+                }
                 posX += 2;
             }
             posY += 1.25f;
         }
+    }
+
+    public PlatformObjectVM[] RandomPlatformPos0(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        int index = Random.Range(0, 2);
+        if (index == 1)
+        {
+            listPlatformInRow[0] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+            for (int k = 1; k < numberPlatformInRow; k++)
+                listPlatformInRow[k] = listPlatform[Random.Range(0, listPlatform.Count)];
+        }
+        else
+        {
+            listPlatformInRow[1] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+            if (numberPlatformInRow == 2)
+                listPlatformInRow[0] = listPlatform[Random.Range(0, listPlatform.Count)];
+            else if (numberPlatformInRow == 3)
+                listPlatformInRow[2] = listPlatform[Random.Range(0, listPlatform.Count)];
+        }
+        return listPlatformInRow;
+    }
+
+    public PlatformObjectVM[] RandomPlatformPos1(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        int index = Random.Range(0, 3);
+        if (index == 1)
+        {
+            listPlatformInRow = HandleRow0(listPlatform, numberPlatformInRow);
+        }
+        else if (index == 2)
+        {
+            listPlatformInRow = HandleRow1(listPlatform, numberPlatformInRow);
+        }
+        else
+        {
+            listPlatformInRow = HandleRow2(listPlatform, numberPlatformInRow);
+        }
+        return listPlatformInRow;
+    }
+
+    public PlatformObjectVM[] RandomPlatformPos2(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        int index = Random.Range(0, 2);
+        if (index == 1)
+        {
+            listPlatformInRow = HandleRow0(listPlatform, numberPlatformInRow);
+        }
+        else
+        {
+            listPlatformInRow = HandleRow1(listPlatform, numberPlatformInRow);
+        }
+        return listPlatformInRow;
+    }
+
+    public PlatformObjectVM[] HandleRow0(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        listPlatformInRow[0] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+        for (int k = 1; k < numberPlatformInRow; k++)
+            listPlatformInRow[k] = listPlatform[Random.Range(0, listPlatform.Count)];
+        return listPlatformInRow;
+    }
+
+    public PlatformObjectVM[] HandleRow1(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        listPlatformInRow[1] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+        if (numberPlatformInRow == 2)
+            listPlatformInRow[0] = listPlatform[Random.Range(0, listPlatform.Count)];
+        else if (numberPlatformInRow == 3)
+            listPlatformInRow[2] = listPlatform[Random.Range(0, listPlatform.Count)];
+        return listPlatformInRow;
+    }
+
+    public PlatformObjectVM[] HandleRow2(List<PlatformObjectVM> listPlatform, int numberPlatformInRow)
+    {
+        var listPlatformInRow = new PlatformObjectVM[3];
+        listPlatformInRow[2] = listPlatform.Where(x => x.IsNormal == true).SingleOrDefault();
+        for (int k = 0; k < numberPlatformInRow - 1; k++)
+            listPlatformInRow[k] = listPlatform[Random.Range(0, listPlatform.Count)];
+        return listPlatformInRow;
     }
 
 }
